fa3dcfbd6703763aa5187c4359a42c4996e92042
Rainnny7 <braydonrainnny@gmail.com>
2024-11-03 01:51:41 -0500

    add a quick link for the admin dash to the nav if logged in

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,9 @@
 import { ReactElement } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { ExternalLink } from "lucide-react";
+import { ExternalLink, Shield } from "lucide-react";
 import { BiLogoDiscordAlt, BiLogoGithub } from "react-icons/bi";
+import { useIsAuthed } from "@/lib/auth";
 
 const links = [
     {
@@ -28,66 +29,79 @@ const socialLinks = [
     },
 ];
 
-const Navbar = (): ReactElement => (
-    <nav className="sticky inset-x-0 top-0 w-full h-[var(--navbar-height)] py-1.5 flex justify-center items-center">
-        <div className="w-full max-w-screen-2xl flex justify-between items-center">
-            {/* Left */}
-            <div className="flex gap-3 sm:gap-7 items-center">
-                {/* Branding */}
-                <Link
-                    className="flex gap-3 items-center hover:opacity-75 transition-all transform-gpu"
-                    href="/"
-                    draggable={false}
-                >
-                    <Image
-                        src="/media/logo.png"
-                        alt="MC Maps Logo"
-                        width={42}
-                        height={42}
-                        draggable={false}
-                    />
-                    <h1 className="hidden sm:flex text-xl font-bold text-primary">
-                        MC Maps
-                    </h1>
-                </Link>
-
-                {/* Links */}
-                <div className="flex gap-4 sm:gap-7 items-center transition-all transform-gpu">
-                    {links.map((link) => {
-                        const external: boolean = link.href.startsWith("http");
-                        return (
-                            <Link
-                                key={link.name}
-                                className="flex gap-1.5 items-center text-sm font-semibold hover:text-[#55FF55]/75 transition-all transform-gpu"
-                                href={link.href}
-                                draggable={false}
-                                target={external ? "_blank" : "_self"}
-                            >
-                                {link.name}
-                                {external && (
-                                    <ExternalLink className="size-3.5" />
-                                )}
-                            </Link>
-                        );
-                    })}
-                </div>
-            </div>
-
-            {/* Right */}
-            <div className="flex gap-2 items-center">
-                {socialLinks.map((link) => (
+const Navbar = async (): Promise<ReactElement> => {
+    const isAuthed: boolean = await useIsAuthed();
+    return (
+        <nav className="sticky inset-x-0 top-0 w-full h-[var(--navbar-height)] py-1.5 flex justify-center items-center">
+            <div className="w-full max-w-screen-2xl flex justify-between items-center">
+                {/* Left */}
+                <div className="flex gap-3 sm:gap-7 items-center">
+                    {/* Branding */}
                     <Link
-                        key={link.name}
-                        className="opacity-75 hover:opacity-100 transition-all transform-gpu"
-                        href={link.href}
-                        target="_blank"
+                        className="flex gap-3 items-center hover:opacity-75 transition-all transform-gpu"
+                        href="/"
                         draggable={false}
                     >
-                        {link.icon}
+                        <Image
+                            src="/media/logo.png"
+                            alt="MC Maps Logo"
+                            width={42}
+                            height={42}
+                            draggable={false}
+                        />
+                        <h1 className="hidden sm:flex text-xl font-bold text-primary">
+                            MC Maps
+                        </h1>
                     </Link>
-                ))}
+
+                    {/* Links */}
+                    <div className="flex gap-4 sm:gap-7 items-center transition-all transform-gpu">
+                        {links.map((link) => {
+                            const external: boolean =
+                                link.href.startsWith("http");
+                            return (
+                                <Link
+                                    key={link.name}
+                                    className="flex gap-1.5 items-center text-sm font-semibold hover:text-[#55FF55]/75 transition-all transform-gpu"
+                                    href={link.href}
+                                    draggable={false}
+                                    target={external ? "_blank" : "_self"}
+                                >
+                                    {link.name}
+                                    {external && (
+                                        <ExternalLink className="size-3.5" />
+                                    )}
+                                </Link>
+                            );
+                        })}
+                    </div>
+                </div>
+
+                {/* Right */}
+                <div className="flex gap-2 items-center divide-x divide-muted">
+                    {/* Socials */}
+                    <div className="flex gap-2 items-center">
+                        {socialLinks.map((link) => (
+                            <Link
+                                key={link.name}
+                                className="opacity-75 hover:opacity-100 transition-all transform-gpu"
+                                href={link.href}
+                                target="_blank"
+                                draggable={false}
+                            >
+                                {link.icon}
+                            </Link>
+                        ))}
+                    </div>
+
+                    {isAuthed && (
+                        <Link href="/admin" draggable={false}>
+                            <Shield className="pl-2 size-7 opacity-75 hover:opacity-100 transition-all transform-gpu" />
+                        </Link>
+                    )}
+                </div>
             </div>
-        </div>
-    </nav>
-);
+        </nav>
+    );
+};
 export default Navbar;
---
+++ src/lib/auth.ts
@@ -0,0 +1,14 @@
+import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+import { cookies } from "next/headers";
+
+/**
+ * Check if the user is authenticated.
+ */
+export const useIsAuthed = async (): Promise<boolean> => {
+    const cookieStore: ReadonlyRequestCookies = await cookies();
+    const session: string | undefined =
+        cookieStore.get("mc-maps-session")?.value;
+    return (
+        session !== undefined && atob(session) === process.env.ADMIN_PASSWORD
+    );
+};
